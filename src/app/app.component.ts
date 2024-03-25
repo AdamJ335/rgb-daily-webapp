e3859e68b8308e15daf41d1d8f8a5e8d9eab6843
AdamJ335 <adamj335@gmail.com>
2024-03-25 18:44:29 +0000

    upgrade cli / pull colour from api request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent implements OnInit {
+  colourApiPackage: any;
 
   title = 'rgb-daily-webapp';
-  redValue = Math.floor(Math.random() * 255); 
-  greenValue = Math.floor(Math.random() * 255); 
-  blueValue = Math.floor(Math.random() * 255); 
+  redValue = this.getValueNumber();
+  greenValue = this.getValueNumber();
+  blueValue = this.getValueNumber();
+  
+  getValueNumber() {
+    return Math.floor(Math.random() * 255);
+  }
+  constructor(private http: HttpClient) { }
 
+  ngOnInit() {
+    this.http.get<any>('https://www.thecolorapi.com/id?rgb=rgb('+this.redValue+','+this.greenValue+','+this.blueValue+')').subscribe(data => {
+      this.colourApiPackage = data.name.value
+    })
+  }
 }
